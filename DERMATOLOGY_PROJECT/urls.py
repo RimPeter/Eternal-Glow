857d6e2014e9042b24a333fe34ab83ae28f36e95
RimPeter <hull@localappliancerentals.co.uk>
2024-09-03 19:37:58 +0100

    Add boostrap
    bootstrap navbar, collectstatic

--- DERMATOLOGY_PROJECT/urls.py
+++ DERMATOLOGY_PROJECT/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('patients/', admin.site.urls),
+    path('', include('patients.urls')),
 ]
