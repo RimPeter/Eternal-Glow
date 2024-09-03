84c75188b742fd41c00d0c53e9396cebdb7895b9
RimPeter <hull@localappliancerentals.co.uk>
2024-09-03 16:13:57 +0100

    Create base and home html

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import PatientInformation
 
-# Register your models here.
+class PatientInformationAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'gender', 'phone_number', 'email')
+    search_fields = ('first_name', 'last_name', 'phone_number', 'email')
+    list_filter = ('gender', 'date_of_birth')
+    ordering = ('last_name', 'first_name')
+    readonly_fields = ('date_created', 'date_updated')
+
+admin.site.register(PatientInformation, PatientInformationAdmin)
