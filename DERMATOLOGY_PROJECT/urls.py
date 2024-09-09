ddb1daeff8f7eb05d8aa68ecdaca464f8f04c1b5
RimPeter <hull@localappliancerentals.co.uk>
2024-09-09 17:48:44 +0100

    Add fontawesom, google fonts and media url in settings

--- DERMATOLOGY_PROJECT/urls.py
+++ DERMATOLOGY_PROJECT/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.urls.conf import include 
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include('client.urls')),
     path('accounts/', include('allauth.urls')),
-    #path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
