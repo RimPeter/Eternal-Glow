ecf683d0594d21a5f7b3d038ca6b0dd1738d4fd4
RimPeter <hull@localappliancerentals.co.uk>
2024-09-09 22:14:48 +0100

    Customize admin

--- treatment/admin.py
+++ treatment/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Product, Category, BodyPart
 
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'category', 'body_part', 'price', 'duration')
+    list_filter = ('category', 'body_part')
+    search_fields = ('product_name', 'category__name', 'body_part__name')
+     
+    ordering = ('category', 'body_part', 'product_name')
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
 admin.site.register(BodyPart)
