c1f5a4a7a078aa5426c5545fb7c2d99cf8387041
RimPeter <hull@localappliancerentals.co.uk>
2024-09-11 06:58:52 +0100

    Add sorting option to all_products.html; sorting by price and duration

--- treatment/views.py
+++ treatment/views.py
@@ -3,7 +3,9 @@ from .models import Product
 from django.db.models import Q
 
 def all_products(request):
-    query = request.GET.get('q')  
+    query = request.GET.get('q') 
+    sort_by = request.GET.get('sort')
+    products = Product.objects.all()
     if query:
         products = Product.objects.filter(
             Q(product_name__icontains=query) | 
@@ -11,6 +13,15 @@ def all_products(request):
         )
     else:
         products = Product.objects.all()
+        
+    if sort_by == 'price_asc':
+        products = products.order_by('price')  
+    elif sort_by == 'price_desc':
+        products = products.order_by('-price')  
+    elif sort_by == 'duration_asc':
+        products = products.order_by('duration') 
+    elif sort_by == 'duration_desc':
+        products = products.order_by('-duration')
     context = {'products': products}
     return render(request, 'treatment/all_products.html', context)
 
