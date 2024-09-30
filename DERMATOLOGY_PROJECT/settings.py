07fd7f8a4c9bb01080ef5bfbfe228c07f349a1c1
RimPeter <hull@localappliancerentals.co.uk>
2024-10-01 00:21:20 +0100

    Create html contents for treatmant app
    anti_aging.html
    injectables.html
    laser.html
    skin.html
    +images

--- DERMATOLOGY_PROJECT/settings.py
+++ DERMATOLOGY_PROJECT/settings.py
@@ -180,7 +180,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
